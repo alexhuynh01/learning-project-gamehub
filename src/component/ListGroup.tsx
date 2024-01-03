a6a474c95086eb65b5ad9fc72dbede5227c34fc0
alexhuynh <121414952+alexhuynh01@users.noreply.github.com>
2024-01-03 14:08:41 +0700

    Update ListGroup.tsx

--- src/component/ListGroup.tsx
+++ src/component/ListGroup.tsx
@@ -1,4 +1,5 @@
 // import { Fragment } from "react";
+import { MouseEvent, useState } from "react";
 
 function ListGroup() {
   let items = [
@@ -8,6 +9,13 @@ function ListGroup() {
     'Saxophone',
     'Guitar'
   ];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //Hook
+  const arr = useState(-1);
+  arr[0] // Variable (selectedIndex)
+  arr[1] // Update function
+
+  const handleClick = (event: MouseEvent) => console.log(event)
   // items = [];
   // const message = items.length == 0 ? <p>Not song found</p> : null
   const message = items.length == 0 && <p>Not song found</p>
@@ -18,12 +26,13 @@ function ListGroup() {
       <h1>List Song</h1>
       { message }
       <ul className="list-group">
-        {/* Ánh xạ danh sách */}
-        {items.map((item, index )=>
+        {/* Ánh xạ danh sách */
+        items.map((item, index )=>
           <li 
-          className="list-group-item" 
+          className={ selectedIndex === index ? "list-group-item active": "list-group-item" }
           key={item} 
-          onClick={() => console.log(item, index+1)}>
+            onClick={()=> {setSelectedIndex(index); }}
+          >
             {item}
             </li>)}
       </ul>
